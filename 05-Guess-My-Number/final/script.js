2e7b7a7dc2420fd6ec434e2c0b891518e3520f44
Mohamed Nizar <nexusnizar@gmail.com>
2022-01-04 10:55:02 +0530

    let added branch 2

--- 05-Guess-My-Number/final/script.js
+++ 05-Guess-My-Number/final/script.js
@@ -84,6 +84,7 @@ document.querySelector('.check').addEventListener('click', function () {
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
+  let chumma=0;
 
   // document.querySelector('.message').textContent = 'Start guessing...';
   displayMessage('Start guessing...');
